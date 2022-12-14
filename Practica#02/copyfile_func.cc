272ed2335c498770b1852d64768202ff0c43f4c8
Raul <alu0101471136@ull.edu.es>
2022-12-14 14:04:01 +0000

    Sexo casi acabado

--- Practica#02/copyfile_func.cc
+++ Practica#02/copyfile_func.cc
@@ -49,38 +49,45 @@ void Usage(int numero_parametros, std::string& primer_argumento) {
 }
 
 std::error_code copy_file(const std::string& src_path, const std::string& dst_path, bool preserve_all=false) {
-  std::error_code error;
-  std::string copia_src_path{src_path};
-  std::string copia_dst_path{dst_path};
-  struct stat comprobacion;
-  if (stat(src_path.c_str(), &comprobacion) == -1 && S_ISREG(comprobacion.st_mode) == 0) {
-    return std::error_code(errno, std::system_category());
-  }
-  if (stat(dst_path.c_str(), &comprobacion)) {
-    assert(src_path != dst_path);
-    if (S_ISDIR(comprobacion.st_mode)) {
-      char* src_file = const_cast<char*>(src_path.c_str());
-      copia_dst_path.append(basename(src_file));
+  try {
+    std::string copia_src_path{src_path};
+    std::string copia_dst_path{dst_path};
+    struct stat src_comprobacion;
+    if (stat(src_path.c_str(), &src_comprobacion) == -1 && S_ISREG(src_comprobacion.st_mode) == 0) {
+      return std::error_code(errno, std::system_category());
     }
+    struct stat dst_comprobacion;
+    if (stat(dst_path.c_str(), &dst_comprobacion)) {
+      assert(src_path != dst_path);
+      if (S_ISDIR(dst_comprobacion.st_mode)) {
+        char* src_file = const_cast<char*>(src_path.c_str());
+        copia_dst_path.append(basename(src_file));
+      }
+    }
+    int src_fd = open(src_path.c_str(), O_RDONLY);
+    int dst_fd = open(copia_dst_path.c_str(), O_CREAT | O_WRONLY, 0666);
+    struct stat dst_copia_comprobacion;
+    if (stat(copia_dst_path.c_str(), &dst_copia_comprobacion)) {
+      open(copia_dst_path.c_str(), O_TRUNC);
+    } else {
+      open(copia_dst_path.c_str(), O_CREAT, 0666);
+    }
+    ssize_t datos;
+    while (datos != 0) {
+      
+    }
+    if (preserve_all) {
+      chmod(copia_dst_path.c_str(), src_comprobacion.st_mode);
+      chown(copia_dst_path.c_str(), src_comprobacion.st_uid, src_comprobacion.st_gid);
+      struct utimbuf buf_time;
+      buf_time.actime = src_comprobacion.st_mtim.tv_sec;
+      buf_time.modtime = src_comprobacion.st_mtim.tv_sec;
+      utime(copia_dst_path.c_str(), &buf_time);
+    } 
+  } catch (const std::exception& error) {
+    std::cerr << "Error copying file" << std::endl;
+    throw error;
   }
-  open(src_path.c_str(), O_RDONLY);
-  open(copia_dst_path.c_str(), O_CREAT | O_WRONLY, 0666);
-  if (stat(copia_dst_path.c_str(), &comprobacion)) {
-    open(copia_dst_path.c_str(), O_TRUNC);
-  } else {
-    open(copia_dst_path.c_str(), O_CREAT, 0666);
-  }
-  ssize_t datos;
-  while (datos != 0) {
-    int fd;
-    datos = read(fd, copia_dst_path, );
-  }
-  if (preserve_all) {
-    chmod(copia_dst_path.c_str(), comprobacion.st_mode);
-    chown(copia_dst_path.c_str(), );
-    const struct utimbuf *buf_time;
-    utime(copia_dst_path.c_str(), buf_time);
-  } 
 }
 
 std::error_code move_file(const std::string& src_path, const std::string& dst_path) {
